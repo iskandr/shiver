019d53be8d758e16c8d706a266da61700e527d0c
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-05-04 23:53:28 -0400

    started making wrapper

--- README.md
+++ README.md
@@ -10,15 +10,15 @@ Example:
 
 ```python
    # call a simple function that takes an index as its only input
-   shiver.run(fn_simple, niters=10)
+   shiver.parfor(fn_simple, niters=10)
    
    # call a function which takes two integers with all pairs 
    # of even integers [2..10] and [10..40]
-   shiver.run(fn_two_inputs, niters = (slice(2,11,2), slice(10,40,2)))
+   shiver.parfor(fn_two_inputs, niters = (slice(2,11,2), slice(10,40,2)))
 
    # more complex function which takes first two fixed array arguments and then
    # a varying integer index which will take values 30,33,36,39, etc... 
    x_gv = GenericValue.pointer(x.ctypes.data)
    y_gv = GenericValue.pointer(y.ctypes.data)
-   shiver.run(fn_three_inputs, fixed_args=[x_gv, y_gv), niters=slice(30,140,3))
+   shiver.parfor(fn_three_inputs, fixed_args=[x_gv, y_gv], niters=slice(30,140,3))
 ```
--- shiver.py
+++ shiver.py
@@ -2,8 +2,11 @@ import math
 import multiprocessing
 import threading
 
-import llvm 
-import llvm.core 
+Module.new('my_module')
+
+from llvm import *
+from llvm.core import * 
+
 
 def savediv(x,y):
   return int(math.ceil(x / float(y)))
@@ -47,16 +50,50 @@ def split_iters(slices, n_pieces):
   # total number of iterations across all variables
   total = reduce(lambda x, y: x*y, counts)
   n_pieces = min(10 * cpu_count(), total)
+
+def return_type(fn):
+  return fn.type.pointee.return_type 
     
-# TODO: Make threads that call ee.run_function with fixed_args + (GenericValue.int(start), etc...) 
-def run(fn, niters, fixed_args = (), ee = None):
-  assert isinstance(fn, llvm.core.Function)
+def mk_wrapper(fn, step_sizes):
+  n_indices = len(step_sizes)
+  m = fn.module
+  ty_int = Type.int(64) 
+  # need to double the integer index inputs to allow for a stop argument 
+  # of each   the number of index inputs to
+  old_input_types = [arg.type for arg in f.args]
+  old_input_names = [arg.name for arg in f.args]
+  extra_index_input_types = [ty_int for _ in xrange(n_indices)]
+  extra_index_names = ["stop%d" % (i+1) for i in xrange(n_indices)]
+  new_input_types = old_input_types + extra_input_types
+  ty_func = Type.function(return_type(fn), new_input_types)
+  new_name = f.name + "_wrapper" 
+  wrapper = fn.module.add_function(ty_func, new_name)
+  for arg_idx in xrange(len(new_input_types)):
+    if arg_idx < len(old_input_types):
+      wrapper.args[arg_idx].name = fn.args[arg_idx].name 
+    else:
+      wrapper.args[arg_idx].name = fn.args[arg_idx - n_indices].name
+  # TODO: create builder, make n_indices loops in the body
+  bb = f_sum.append_basic_block("entry")
+  builder = Builder.new(bb)
+  
+  return wrapper 
+
+ 
+def parfor(fn, niters, fixed_args = (), ee = None):
+  assert isinstance(fn, Function), \
+    "Can only run LLVM functions, not %s" % type(fn)
+  assert return_type(fn) == Type.void, \
+    "Body of parfor loop must return void, not %s" % return_type(fn) 
   slices = parse_iters(niters)
-  assert len(fn.args) == len(fixed_args) + len(slices)  
+  n_fixed = len(fixed_args)
+  n_indices = len(slices)
+  n_args = n_fixed + n_indices 
+  assert len(fn.args) == n_args
   split_slices = split_iters(slices)   
   if ee is None:
     ee = llvm.ee.ExecutionEngine.new(fn.module)
-  
-     
+  work_fn = mk_wrapper(fn, n_indices) 
+   
     
   
