2f35549cf5087950d2ac378921f90cfacc63bce6
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2013-05-06 12:15:30 -0400

    first test actually generates code

--- llvm_helpers.py
+++ llvm_helpers.py
@@ -4,6 +4,7 @@ import numpy as np
 from llvm import * 
 from llvm.core import * 
 
+ty_void = Type.void()
 ty_int8 = Type.int(8)
 ty_int16 = Type.int(16) 
 ty_int32 = Type.int(32) 
@@ -18,7 +19,7 @@ ty_ptr_int32 = Type.pointer(ty_int32)
 ty_ptr_int64 = Type.pointer(ty_int64)
 
 ty_ptr_float32 = Type.pointer(ty_float32)
-ty_ptr_float64 = Type.double(ty_float64)
+ty_ptr_float64 = Type.pointer(ty_float64)
 
 mappings = {
   np.int8 : ty_int8,             
@@ -50,3 +51,94 @@ def to_lltype(t):
     return ptr(elt_t) 
   assert t in mappings, "Unknown type %s" % (t,)
   return mappings[t]
+
+
+def return_type(fn):
+  return fn.type.pointee.return_type 
+
+
+
+class LoopBuilder(object):
+  def __init__(self, wrapper, original_fn, closure_values, 
+               start_values, stop_values, step_constants):
+    self.wrapper = wrapper 
+    self.original_fn = original_fn 
+    self.closure_values = closure_values 
+    self.start_values = start_values 
+    self.stop_values = stop_values 
+    self.step_constants = step_constants 
+    self.n_loops = len(start_values)
+    assert len(stop_values) == self.n_loops 
+    assert len(step_constants) == self.n_loops 
+    n_args = len(original_fn.args)
+    assert len(closure_values) + self.n_loops == n_args
+  
+  _loop_var_names = ['i', 'j', 'k', 'l']
+  
+  def new_block(self, name):
+    bb = self.wrapper.append_basic_block(name)
+    builder = Builder.new(bb)
+    return bb, builder
+  
+  def get_var_name(self, pos):
+    maxpos = len(self._loop_var_names)
+    return self._loop_var_names[pos % maxpos] * ((pos/maxpos)+1)
+  
+  def create(self):
+    bb, builder = self.new_block("entry")
+    return self._create(bb,builder)
+  
+  def _create(self, bb, builder, loop_vars = []):
+    n = len(loop_vars)
+    if n == self.n_loops:
+      builder.call(self.original_fn, self.closure_values + loop_vars)
+      return builder
+    else:
+      var = builder.alloca(ty_int64, self.get_var_name(n))
+     
+      builder.store(self.start_values[n],var)
+      test_bb, test_builder =  self.new_block("test%d" % (n+1))
+      builder.branch(test_bb)
+      idx_value = test_bb.load(var)                         
+      cond = builder.icmp(ICMP_ULT, idx_value,self.stop_values[n], "stop_cond%d" % (n+1))
+      body_bb, body_builder = self.new_block("body%d" % (n+1))
+      after_bb, after_builder = self.new_block("after_loop%d" % (n+1))
+      test_builder.cbranch(cond, body_bb, after_bb)
+      body_builder = self.create(body_bb, body_builder, loop_vars + [var])
+      body_builder.branch(test_builder)
+      return after_builder 
+    
+def empty_fn(module, name, input_types, output_type = ty_void):
+  ty_func = Type.function(output_type, input_types)
+  return module.add_function(ty_func, name)  
+
+def mk_wrapper(fn, step_sizes):
+  n_indices = len(step_sizes)
+
+  # need to double the integer index inputs to allow for a stop argument 
+  # of each   the number of index inputs to
+  old_input_types = [arg.type for arg in fn.args]
+  extra_input_types = [ty_int64 for _ in xrange(n_indices)]
+  new_input_types = old_input_types + extra_input_types
+  wrapper = empty_fn(fn.module, fn.name + "_wrapper",  new_input_types)
+  
+  for arg_idx in xrange(len(new_input_types)):
+    if arg_idx < len(old_input_types):
+      wrapper.args[arg_idx].name = fn.args[arg_idx].name 
+    else:
+      wrapper.args[arg_idx].name = fn.args[arg_idx - n_indices].name + "_stop"
+  index_vars = wrapper.args[-2*n_indices:]
+  closure_vars = wrapper.args[:-2*n_indices]
+  start_vars = index_vars[:n_indices]
+  stop_vars = index_vars[n_indices:]
+  
+  assert len(start_vars) == len(stop_vars)
+  loop_builder = LoopBuilder(wrapper, 
+                             fn, 
+                             closure_vars, 
+                             start_vars, 
+                             stop_vars, 
+                             step_sizes)
+  exit_builder = loop_builder.create()
+  exit_builder.ret()
+  return wrapper 
--- test.py
+++ test.py
@@ -1,10 +1,27 @@
 import numpy as np
 
-from llvm import * 
-from llvm.core import * 
+import llvm 
+import llvm.core 
 import llvm.ee as ee 
 
 import shiver
+import llvm_helpers
+from llvm_helpers import  ty_int64, empty_fn 
 
-def test_zero_iters():
-  shiver.parfor(None, niters=0)
+global_module = llvm.core.Module.new("global_module")
+
+def mk_identity_fn():
+  fn = empty_fn(global_module, "ident", (ty_int64,), ty_int64  )
+  bb = fn.append_basic_block("entry")
+  builder = llvm.core.Builder.new(bb)
+  builder.ret(fn.args[0])
+  return fn 
+
+ident = mk_identity_fn()
+
+def test_should_fail():
+  try:
+    shiver.parfor(ident, niters=1)
+    assert False, "Shouldn't have accepted function with non-void return"
+  except:
+    pass 
